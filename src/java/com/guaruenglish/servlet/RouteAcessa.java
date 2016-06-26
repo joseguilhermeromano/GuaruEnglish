8e93d920c7b4a42a2e1fe14a08356f676a843ca3
Luiz Felipe <Luiz Felipe@Helumn>
2016-06-26 02:40:16 -0300

    adição de um page router

---
+++ src/java/com/guaruenglish/servlet/RouteAcessa.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guaruenglish.servlet;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Luiz Felipe
+ */
+public class RouteAcessa implements Tarefa{
+    
+    private final static Map<String,String> PAGINA = new HashMap();
+        static {
+            PAGINA.put("cadastrarUsuario", "WEB-INF/Paginas/secretaria/cadastrarUsuario.jsp");
+        }
+    
+    @Override
+    public String executa(HttpServletRequest req, HttpServletResponse resp) {
+        String pagina = PAGINA.get(req.getParameter("action"));
+        if(pagina != null)
+            return pagina;
+        return "index.jsp";
+    }
+    
+    
+}
