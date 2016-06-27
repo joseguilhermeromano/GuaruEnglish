e2fb2358a334bb532ad9d0e0394da600ad6397b2
Luiz Felipe <Luiz Felipe@Helumn>
2016-06-27 00:34:58 -0300

    adição do genaretedValue

--- src/java/com/guaruenglish/model/Parcela.java
+++ src/java/com/guaruenglish/model/Parcela.java
@@ -7,6 +7,8 @@ package com.guaruenglish.model;
 
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -20,6 +22,7 @@ import javax.persistence.Table;
 @Table(name = "parcela")
 public class Parcela {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private int status;
     private Date dataPagamento;
