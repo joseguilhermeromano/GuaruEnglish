9b624fe5417b29fe97a235707ac1cf10b68f78f9
Luiz Felipe <Luiz Felipe@Helumn>
2016-06-29 23:24:49 -0300

    adição do método de consulta boletim e consulta boletim por turma

--- src/java/com/guaruenglish/dao/BoletimDAO.java
+++ src/java/com/guaruenglish/dao/BoletimDAO.java
@@ -5,13 +5,46 @@
  */
 package com.guaruenglish.dao;
 
+import com.guaruenglish.model.Boletim;
+import com.guaruenglish.util.JPAutil;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 /**
  *
  * @author Luiz Felipe
  */
 public class BoletimDAO {
     
-    public void cadastrarBoletim() {
+    EntityManager entityManager = new JPAutil().getEntityManager();
+    
+    public void insereBoletim(Boletim boletim) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(boletim);
+            entityManager.getTransaction().commit();
+            entityManager.close();
+        } catch(Exception e) {
+            entityManager.getTransaction().rollback();
+        }
+    }
+    
+    public Boletim consultaBoletim(int idAluno, int idTurma) {
+        try {
+            Query query = entityManager.createQuery("SELECT b FROM Boletim b "
+                    + "WHERE b.turma.id='"+idTurma+"' "
+                    + "AND b.aluno.id='"+idAluno+"'");
+            Boletim boletim = (Boletim) query.getSingleResult();
+            return boletim;
+        } catch(Exception e) {
+            return null;
+        }
+    }
+    
+    public List<Boletim> consultaBoletimTurma(int ) {
         
     }
+    
 }
