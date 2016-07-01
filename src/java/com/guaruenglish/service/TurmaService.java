56c114f2e4b03f257c702d096f7254b3db599782
Luiz Felipe <Luiz Felipe@Helumn>
2016-07-01 11:02:10 -0300

    adição do método que irá associar o professor a turma

--- src/java/com/guaruenglish/service/TurmaService.java
+++ src/java/com/guaruenglish/service/TurmaService.java
@@ -5,7 +5,9 @@
  */
 package com.guaruenglish.service;
 
+import com.guaruenglish.dao.ProfessorDAO;
 import com.guaruenglish.dao.TurmaDAO;
+import com.guaruenglish.model.Professor;
 import com.guaruenglish.model.Turma;
 import java.util.List;
 
@@ -20,9 +22,33 @@ public class TurmaService {
         return new TurmaDAO().buscarTurmasAtivadasSemProfessor();
     }
     
+    
+    public boolean associaProfessorTurma(int idProfessor, int idTurma) {
+        if(idProfessor == 0 || idTurma == 0)
+            return false;
+        
+        Professor professor = new ProfessorDAO().buscaProfessor(idProfessor);
+        Turma turma = new TurmaDAO().buscaTurma(idTurma);
+        
+        //Falta Colocar a condição para verficar se o professor pode lecionar essa turma.
+        // para isso é melhor trocar o nivel de experiencia do professor para int
+        
+        turma.setProfessor(professor);
+        
+        boolean resposta = new TurmaDAO().alteraTurma(turma);
+        
+        return resposta;    
+        
+    }
+    
     public Turma consultaTurma(int id) {
         return new TurmaDAO().buscaTurma(id);
     }
+
+    private Object TurmaDAO() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
     
 }
